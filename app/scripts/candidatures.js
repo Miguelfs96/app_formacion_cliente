aa5df2988460bb7faa99e33609fc1a8129b86563
Miguel <fernandez.s.miguel96@gmail.com>
2020-04-28 09:07:01 +0200

    documentacion candidatures

--- app/scripts/candidatures.js
+++ app/scripts/candidatures.js
@@ -5,7 +5,16 @@ const generateHeaderCell = () => document.createElement('th');
 const generateRow = () => document.createElement('tr');
 const generateCell = () => document.createElement('td');
 
-//FUNCION PRINCIPAL
+//VARIABLES GLOBALES
+const search = {};
+
+
+//FUNCIONES PRINCIPALES
+
+/**
+ * Genera la tabla candidatos y la pinta por pantalla a partir del json
+ * y una fila de busqueda
+ */
 export const generateTableCandidates = () => {
   let tabla = document.getElementById("tablaPrincipal");
   let tbody = document.createElement('tbody');
@@ -24,8 +33,59 @@ export const generateTableCandidates = () => {
   tabla.appendChild(tbody);
 }
 
+/**
+ * Elastic search para buscar la por una o varias columnas
+ * @param {evento} e
+ */
+const busqueda = (e) => {
+  const key = e.target.name; //nombre de la propiedad
+
+  const value = document.getElementById(key).value;
+
+  search[key] = value;
+
+  const result = data.candidatures.filter(candidate => {
+    let isValid = true;
+    Object.keys(search).forEach(key => {
+      if (key === "titulacionPracticas") {
+        if (!new RegExp(`${search[key]}`, "i").test(candidate[key].name)) {
+          isValid = false;
+        }
+      }else if (!new RegExp(`${search[key]}`, "i").test(candidate[key])) {
+        isValid = false;
+      }
+    });
+    return isValid;
+
+  });
+  reprint(result);
+};
+
+/**
+ * Repinta el cuerpo de la tabla a partir de un array de objetos
+ * @param {Array} resultados
+ */
+const reprint = (resultados) => {
+  let tbody = document.getElementById('tableBody');
+  tbody.innerText = '';
+
+   resultados.reduce(reducidor, [])
+    .forEach(element => {
+      let fila = generateRow();
+      printCell(element, fila);
+      tbody.appendChild(fila);
+    });
+
+}
+
 
 //FUNCIONES AUXILIARES
+
+/**
+ * Genera una celda y la agrrega al nodo fila
+ * @param {Object} element
+ * @param {Node} fila
+ */
 const printCell = (element, fila) => {
   for (const prop in element) {
     let cell = generateCell();
@@ -34,19 +94,20 @@ const printCell = (element, fila) => {
   }
 }
 
+/**
+ * Resetea la tabla por completo
+ * @param {Node} table
+ */
 const resetTable = (table) => {
   table.textContent = '';
 }
 
-const reducidor = (accum, {
-  dataPresentacion,
-  name,
-  surname,
-  dni,
-  telf,
-  email,
-  titulacionPracticas
-}) => {
+/**
+ * Funcion que transforma un objeto a un array de objetos clave-valor
+ * @param {Array} accum
+ * @param {Object} param1
+ */
+const reducidor = (accum, {dataPresentacion, name, surname, dni, telf, email, titulacionPracticas}) => {
   accum.push({
     dataPresentacion,
     name: `${name} ${surname}`,
@@ -59,6 +120,10 @@ const reducidor = (accum, {
 
 }
 
+/**
+ * Genera el encabezado de la tabla y no agrega al nodo
+ * @param {Node} table
+ */
 const headerTable = (table) => {
   let fila = generateRow();
   let array = ["Fecha", "Nombre", "DNI", "Tel", "Email", "Titulo"];
@@ -70,9 +135,14 @@ const headerTable = (table) => {
   table.appendChild(fila);
 }
 
+
+/**
+ * Genera la fila de filtros en una tabla y la agrega al nodo
+ * @param {nodo} table
+ */
 const filtros = (table) => {
   let fila = generateRow();
-  let array = ["dataPresentacion", "name", "dni", "telf", "email", "titulacionPracticas"]; // QUIZA HAYA QUE CAMBIARLO
+  let array = ["dataPresentacion", "name", "dni", "telf", "email", "titulacionPracticas"]; // QUIZA HAYA QUE CAMBIARLO O GENERARLOS DE OTRA FORMA
 
   const generateInput = () => document.createElement('input')
 
@@ -90,44 +160,3 @@ const filtros = (table) => {
 
   table.appendChild(fila);
 }
-
-
-
-const search = {};
-
-const busqueda = (e) => {
-  const key = e.target.name; //nombre de la propiedad
-
-  const value = document.getElementById(key).value;
-
-  search[key] = value;
-
-  const result = data.candidatures.filter(candidate => {
-    let isValid = true;
-    Object.keys(search).forEach(key => {
-      if (!new RegExp(`${search[key]}`, "i").test(candidate[key])) {
-        isValid = false;
-      }
-    });
-    return isValid;
-
-  });
-  reprint(result);
-
-};
-
-
-const reprint = (resultados) => {
-
-  let tbody = document.getElementById('tableBody');
-  tbody.innerText = '';
-
-   resultados.reduce(reducidor, [])
-    .forEach(element => {
-      let fila = generateRow();
-      printCell(element, fila);
-      tbody.appendChild(fila);
-    });
-
-}
-
