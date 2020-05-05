bd48ea00f58fa9b1371060593f1c8fbfbaaa8b86
Miguel <fernandez.s.miguel96@gmail.com>
2020-05-05 09:59:29 +0200

    menu detalle & estilo formulario

--- app/scripts/contenidoDetalleCandidatura.js
+++ app/scripts/contenidoDetalleCandidatura.js
@@ -8,18 +8,32 @@ const resetMainContain = () => {
  * @param {array} data
  */
 export const generarDetails = (e, data) => {
-
   resetMainContain();
-
   const idValue = e.currentTarget.firstChild.textContent;
-
   let elemento = data.find(element => (element.idCandidate == idValue || element.idInternshipOffer == idValue));
-
   const formulario = document.createElement('form'); //generacion de formulario
-
   generarImputs(elemento, formulario);
-
+  generateNavegation(data);
   document.getElementById("container").appendChild(formulario);
+}
+
+
+const generateNavegation = (data) =>{
+  const botones = ["Informacion Personal", "Titulaciones", "Direcciones"];
+  let navBarB = document.createElement('nav');
+  navBarB.setAttribute('id', 'navBarB');
+  let listNavB = document.createElement('ul')
+  listNavB.setAttribute('id', 'navForm');
+  botones.forEach(element => {
+    let elementNavB = document.createElement('li');
+    let buttonNav = document.createElement('button');
+
+    buttonNav.innerText = element;
+    elementNavB.appendChild(buttonNav);
+    listNavB.appendChild(elementNavB);
+  })
+  navBarB.appendChild(listNavB);
+  document.getElementById("container").appendChild(navBarB);
 }
 
 /**
@@ -30,21 +44,20 @@ export const generarDetails = (e, data) => {
 const generarImputs = (object, formulario) =>{
   for (const key in object) {
     if (object.hasOwnProperty(key)) {
-      const element = object[key];
-      if (typeof element=== "object") {
-        generarImputs(element, formulario);
-      }else{
+      if (typeof object[key] != "object") {
+      const divForm = document.createElement('div');
       const labelForm = document.createElement('label');
       const inputForm = document.createElement('input');
       labelForm.setAttribute('for', `${key}Form`);
       labelForm.innerText = `${key}`;
-      formulario.appendChild(labelForm);
 
-      inputForm.setAttribute('id', `${key}Form`)
+      inputForm.setAttribute('id', `${key}Form`);
       inputForm.setAttribute('name', `${key}`);
       inputForm.setAttribute('value', `${object[key]}`);
-      formulario.appendChild(labelForm);
-      formulario.appendChild(inputForm);
-    }}
+      divForm.appendChild(labelForm);
+      divForm.appendChild(inputForm);
+      formulario.appendChild(divForm);
+    }
+  }
   }
 }
